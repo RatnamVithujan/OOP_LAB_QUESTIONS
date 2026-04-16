2b6df89d697efa65ef357d155eed2e7e9d6575bb
RatnamVithujan <vithumail5@gmail.com>
2026-04-16 23:30:48 +0530

    Update StaffMember.java

--- OOP_LAB/src/main/java/LAB4/StaffMember.java
+++ OOP_LAB/src/main/java/LAB4/StaffMember.java
@@ -1,8 +1,13 @@
 package LAB4;
 
 abstract class StaffMember{
-    private final String staffId = null;
+    private final String staffId;
     private String fullName;
     protected String department;
     
+    public StaffMember(String staffId, String fullName , String department){
+        this.staffId = staffId;
+        this.fullName = fullName;
+        this.department = department;
+    }
 }
