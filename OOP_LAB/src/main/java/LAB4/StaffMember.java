76c148375ad00138a33c98c99420c71ca3a3cbc9
RatnamVithujan <vithumail5@gmail.com>
2026-04-16 23:10:09 +0530

    Create StaffMember.java

---
+++ OOP_LAB/src/main/java/LAB4/StaffMember.java
@@ -0,0 +1,11 @@
+package LAB4;
+
+abstract class StaffMember{
+    private final String staffId;
+    private String fullName;
+    protected String department;
+    
+}
+public class StaffMember(String) {
+    
+}
