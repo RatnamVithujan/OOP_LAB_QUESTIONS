786b9667e042706d1710113f0c069f9a87815151
RatnamVithujan <vithumail5@gmail.com>
2026-04-16 23:10:24 +0530

    Create OOP_LAB.java

---
+++ OOP_LAB/src/main/java/com/mycompany/oop_lab/OOP_LAB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop_lab;
+
+/**
+ *
+ * @author VITHU
+ */
+public class OOP_LAB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
