bdc320da34a0b062c3cfafed652069eb42bd9fc3
RatnamVithujan <vithumail5@gmail.com>
2026-04-17 10:50:36 +0530

    Update StaffMember.java

--- OOP_LAB/src/main/java/LAB4/StaffMember.java
+++ OOP_LAB/src/main/java/LAB4/StaffMember.java
@@ -1,13 +1,33 @@
 package LAB4;
 
 abstract class StaffMember{
-    private final String staffId;
+    private final String staffID;
     private String fullName;
     protected String department;
     
-    public StaffMember(String staffId, String fullName , String department){
-        this.staffId = staffId;
+    public StaffMember(String staffID, String fullName , String department){
+        this.staffID = staffID;
         this.fullName = fullName;
         this.department = department;
     }
+    
+    public String getstaffID(){
+        return staffID;
+    }
+    public String getfullName(){
+        return fullName;
+    }
+    public String getdepartment(){
+        return department;
+    }
+    
+    public final void displayBasicDetails(){
+        System.out.println("Staff ID: " + staffID);
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Department: " + department);
+    }
+    
+    public abstract double calculateMonthlyPayment();
+    
+    //Why this is abstract because prevent the incomplete objects
 }
