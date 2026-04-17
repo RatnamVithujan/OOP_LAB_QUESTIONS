39977db70eb7d99d3578ed64786347ac2ad67131
RatnamVithujan <vithumail5@gmail.com>
2026-04-17 14:12:09 +0530

    Update Lab04Main.java

--- OOP_LAB/src/main/java/LAB4/Lab04Main.java
+++ OOP_LAB/src/main/java/LAB4/Lab04Main.java
@@ -14,17 +14,19 @@ public class Lab04Main {
         Lecturer lec2 = new Lecturer("L002","Dr. Prabth", "Physics", 2, 25000.0);
         LabAssistant assistant1 = new LabAssistant("A001","Ratnam Vithujan", "Electronics", 40, 250.0);
         //==================================================================================================
-        System.out.println("Total Number of Staffs: " + StaffMember.getStaffCount());
+        lec1.changeDepartment("Electronics");
         
         System.out.println("--- Lecturer Information ---");
         lec1.displayLecturerDetails();
         System.out.println("Total Monthly Payment: " + lec1.calculateMonthlyPayment());
         System.out.println("Estimated Bonus: " + UniversityPolicy.calculateBonus(lec1.calculateMonthlyPayment()));
+        lec1.showCommonNotice();
         
         System.out.println();
         lec2.displayLecturerDetails();
         System.out.println("Total Monthly Payment: " + lec2.calculateMonthlyPayment());
         System.out.println("Estimated Bonus: " + UniversityPolicy.calculateBonus(lec2.calculateMonthlyPayment()));
+        lec2.showCommonNotice();
         
         System.out.println("\n------------------------\n");
         
@@ -32,5 +34,18 @@ public class Lab04Main {
         assistant1.displayLabAssistantDetails();
         System.out.println("Total Monthly Payment: " + assistant1.calculateMonthlyPayment());
         System.out.println("Estimated Bonus: " + UniversityPolicy.calculateBonus(assistant1.calculateMonthlyPayment()));
+        assistant1.showCommonNotice();
+        
+        System.out.println();
+        System.out.println("Total Number of Staffs: " + StaffMember.getStaffCount());
+        System.out.println();
+        StaffMember[] staffList = {lec1,lec2,assistant1};
+        double total =0;
+        for(int i = 0; i < staffList.length; i++){
+            double basePay = staffList[i].calculateMonthlyPayment();
+            double bonus = UniversityPolicy.calculateBonus(basePay);
+            total += (basePay + bonus);
+        }
+        System.out.println("Total Cost Per Month: " + total);
     }
 }
