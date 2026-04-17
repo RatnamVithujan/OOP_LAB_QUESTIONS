cf04b0936ba21c087f2f29810f5566bbd675c6d3
RatnamVithujan <vithumail5@gmail.com>
2026-04-17 12:36:10 +0530

    Update Lab04Main.java

--- OOP_LAB/src/main/java/LAB4/Lab04Main.java
+++ OOP_LAB/src/main/java/LAB4/Lab04Main.java
@@ -2,17 +2,24 @@ package LAB4;
 
 public class Lab04Main {
     public static void main(String[] args){
+        
+        UniversityPolicy.showPolicyHeader();
+        System.out.println("University Name: " + UniversityPolicy.UNIVERSITY_NAME);
+        System.out.println();
+        
         Lecturer lec1 = new Lecturer("L001","Dr. Jehan", "Computer Science", 3, 5000.0);
         LabAssistant assistant1 = new LabAssistant("A001","Ratnam Vithujan", "Electronics", 40, 250.0);
         
         System.out.println("--- Lecturer Information ---");
         lec1.displayLecturerDetails();
         System.out.println("Total Monthly Payment: " + lec1.calculateMonthlyPayment());
+        System.out.println("Estimated Bonus: " + UniversityPolicy.calculateBonus(lec1.calculateMonthlyPayment()));
         
         System.out.println("\n------------------------\n");
         
         System.out.println("--- Lab Assistant Details ---");
         assistant1.displayLabAssistantDetails();
         System.out.println("Total Monthly Payment: " + assistant1.calculateMonthlyPayment());
+        System.out.println("Estimated Bonus: " + UniversityPolicy.calculateBonus(assistant1.calculateMonthlyPayment()));
     }
 }
